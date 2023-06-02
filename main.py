5f8cdc7f9cfdc487de8235ab69adc10b7e3d1290
O Shah <oshah81@outlook.com>
2023-06-02 21:42:58 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from program import program
+
+def main():
+    # Edit your main.py to match the configuration of your hardware
+    program("SSID1", "password", "C3")
+
+
+if __name__ == "__main__":
+    main()
