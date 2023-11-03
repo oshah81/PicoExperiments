da80e69696b3e4c2ee4be9f73a04b42c5044a6c7
oshah81 <oshah81@outlook.com>
2023-11-03 14:56:52 +0000

    Main in ignore list

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from program import program
 
-def main():
-    # Edit your main.py to match the configuration of your hardware
-    program("SSID1", "password", "C3")
-
-
-if __name__ == "__main__":
-    main()
+program("RPI32266876", "hpiph0nemasrock0", "C1")
+#       
